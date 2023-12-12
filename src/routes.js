c1b88a54d53d10e0c81b01be1224721b6c0ddd07
Miqueias Mesquita <miqueias_jed@hotmail.com>
2023-12-12 18:20:55 -0300

    search

--- src/routes.js
+++ src/routes.js
@@ -9,9 +9,15 @@ export const routes = [
         method: 'GET',
         path: buildRoutePath('/users'),
         handler: (req, res) => {
-            const users = database.select('users')
 
-            return res.end(JSON.stringify(users));
+            const { search } = req.query
+
+            const users = database.select('users', search ? {
+                name: search,
+                email: search
+            } : null)
+
+            return res.end(JSON.stringify(users))
         }
     },
     {
